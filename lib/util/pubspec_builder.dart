2dad400a0dc97a12c05a2c74e591a6962eb5a71b
SleepySquash <NordNikita@icloud.com>
2022-06-29 13:20:13 +0300

    Fix copyright

--- lib/util/pubspec_builder.dart
+++ lib/util/pubspec_builder.dart
@@ -1,3 +1,19 @@
+// Copyright © 2022 IT ENGINEERING MANAGEMENT INC, <https://github.com/team113>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License v3.0 as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License v3.0 for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License v3.0
+// along with this program. If not, see
+// <https://www.gnu.org/licenses/agpl-3.0.html>.
+
 import 'dart:async';
 
 import 'package:build/build.dart';
@@ -9,10 +25,10 @@ Builder pubspecBuilder(BuilderOptions options) {
   return PubspecBuilder(options);
 }
 
-/// [Builder] generating a `lib/pubspec.g.dart` file containg the package's name
-/// and version.
+/// [Builder] generating a `lib/pubspec.g.dart` file containing the package's
+/// name and version.
 class PubspecBuilder implements Builder {
-  PubspecBuilder(this.builderOptions);
+  const PubspecBuilder(this.builderOptions);
 
   /// Configuration of this [PubspecBuilder].
   final BuilderOptions builderOptions;
