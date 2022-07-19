03569206d547842b3accd34e5a606427906d8f1b
andrii-heletkanych <43937728+andrigel@users.noreply.github.com>
2022-07-19 15:52:20 +0300

    Corrections

--- lib/ui/page/home/controller.dart
+++ lib/ui/page/home/controller.dart
@@ -20,9 +20,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-import '/domain/model/application_settings.dart';
 import '/domain/model/my_user.dart';
-import '/domain/repository/settings.dart';
 import '/domain/service/auth.dart';
 import '/domain/service/my_user.dart';
 import '/routes.dart';
