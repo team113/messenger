0353dbb02b04f3dd774f80a3e5fbe0fb46fe45b2
LisisFd <lisis.fd@gmail.com>
2022-08-05 18:12:14 +0300

    create and testing AvatarImage

--- lib/ui/page/home/widget/avatar.dart
+++ lib/ui/page/home/widget/avatar.dart
@@ -18,7 +18,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
+import 'package:gif/gif.dart';
 import '/config.dart';
 import '/domain/model/avatar.dart';
 import '/domain/model/chat.dart';
@@ -261,16 +261,16 @@ class AvatarWidget extends StatelessWidget {
           ),
           image: avatar == null
               ? null
-              : DecorationImage(
+              : const DecorationImage(
                   image: NetworkImage(
-                    '${Config.url}:${Config.port}/files${avatar?.original}',
+                    'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/5eeea355389655.59822ff824b72.gif',
                   ),
                   fit: BoxFit.cover,
                   isAntiAlias: true,
                 ),
           shape: BoxShape.circle,
         ),
-        child: avatar == null
+        child: avatar != null
             ? LayoutBuilder(builder: (context, constraints) {
                 return Center(
                   child: Text(
@@ -283,7 +283,9 @@ class AvatarWidget extends StatelessWidget {
                   ),
                 );
               })
-            : null,
+            : ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: const AvatarImage()),
       );
     });
   }
@@ -328,5 +330,47 @@ extension _LightenColorExtension on Color {
         hsl.withLightness((hsl.lightness + amount).clamp(0.0, 1.0));
 
     return hslLight.toColor();
+  }
+}
+
+class AvatarImage extends StatefulWidget {
+  const AvatarImage({Key? key}) : super(key: key);
+
+  @override
+  State<AvatarImage> createState() => _AvatarImageState();
+}
+
+class _AvatarImageState extends State<AvatarImage>
+    with TickerProviderStateMixin {
+  late GifController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = GifController(vsync: this);
+  }
+
+  void onEntered(bool isHovered){
+    isHovered?_controller.repeat() : _controller.reset();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+        _controller.forward(from: 0);
+      },
+      child: MouseRegion(
+        onEnter: (_)=> onEntered(true),
+        onExit: (_)=> onEntered(false),
+        child: Gif(
+          image: const NetworkImage(
+              'https://gapopa.net/files/47/17/35/83/bb/de/a3/f1/51/2d/d6/2d/6a/8f/31/ac/orig.gif'),
+          controller: _controller,
+          autostart: Autostart.once,
+        ),
+      ),
+    );
   }
 }
