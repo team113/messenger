d95bb57bb8dc2663078fd8a5b292cc92cfe727cd
skazkiful <qwe1233qwe123@gmail.com>
2022-10-07 11:44:45 +0300

    some working version

--- lib/ui/page/home/widget/retry_image.dart
+++ lib/ui/page/home/widget/retry_image.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
 
+/// Widget that trying to load image and display it.
 class RetryImage extends StatefulWidget {
   const RetryImage(
     this.url, {
@@ -34,13 +34,16 @@ class RetryImage extends StatefulWidget {
   State<RetryImage> createState() => _RetryImageState();
 }
 
+/// [State] of [RetryImage].
 class _RetryImageState extends State<RetryImage> {
-  ///
+  /// [Timer] of backOff loading image.
   Timer? _timer;
 
+  /// Indicator whether image was loaded or not.
   bool _loaded = false;
+
+  /// [Uint8List] image bytes.
   late Uint8List _image;
-  bool error403WasCalled = false;
 
   /// Duration of backOff loading image.
   int _reconnectPeriodMillis = 500 ~/ 2;
@@ -65,9 +68,12 @@ class _RetryImageState extends State<RetryImage> {
           height: widget.height,
           fit: widget.fit,
         )
-      : Center(
+      : Container(
+          height: widget.height,
+          width: widget.width,
+          alignment: Alignment.center,
           child: Container(
-            constraints: const BoxConstraints(maxHeight: 50, maxWidth: 50),
+            constraints: const BoxConstraints(maxHeight: 40, maxWidth: 40),
             child: const AspectRatio(
               aspectRatio: 1 / 1,
               child: CircularProgressIndicator(),
@@ -75,15 +81,15 @@ class _RetryImageState extends State<RetryImage> {
           ),
         );
 
+  /// Trying to load image.
   Future<void> _loadImage() async {
     http.Response? data;
     try {
       data = await http.get(Uri.parse(widget.url));
     } catch (e) {
-      print(e);
+      // No-op.
     }
-    if (data?.statusCode == 403 && error403WasCalled == false) {
-      error403WasCalled = true;
+    if (data?.statusCode == 403) {
       widget.error403?.call();
       if (mounted) {
         setState(() {});
