e9bd9edd8e5f3c5069b63bb556a83f36eeea5734
skazkiful <30957783+skazkiful@users.noreply.github.com>
2022-11-11 13:21:13 +0200

    Implement drafts in `Chat` (#173, #102)
    Additionally:
    - fix `GalleryPopup` incorrect behaviour with attached `Attachment`s in `Chat` 
    - fix `router` late initialization error due to `NotificationService`
    - fix `TextFieldState.text` not setting its previous value
    
    Co-authored-by: SleepySquash <nordnikita@icloud.com>

--- test/widget/chat_hide_test.dart
+++ test/widget/chat_hide_test.dart
@@ -39,6 +39,7 @@ import 'package:messenger/provider/hive/chat.dart';
 import 'package:messenger/provider/hive/chat_call_credentials.dart';
 import 'package:messenger/provider/hive/chat_item.dart';
 import 'package:messenger/provider/hive/contact.dart';
+import 'package:messenger/provider/hive/draft.dart';
 import 'package:messenger/provider/hive/gallery_item.dart';
 import 'package:messenger/provider/hive/media_settings.dart';
 import 'package:messenger/provider/hive/session.dart';
@@ -123,6 +124,9 @@ void main() async {
   var settingsProvider = MediaSettingsHiveProvider();
   await settingsProvider.init();
   await settingsProvider.clear();
+  var draftProvider = Get.put(DraftHiveProvider());
+  await draftProvider.init();
+  await draftProvider.clear();
   var applicationSettingsProvider = ApplicationSettingsHiveProvider();
   await applicationSettingsProvider.init();
   var backgroundProvider = BackgroundHiveProvider();
@@ -245,6 +249,7 @@ void main() async {
         graphQlProvider,
         chatProvider,
         callRepository,
+        draftProvider,
         userRepository,
       ),
     );
