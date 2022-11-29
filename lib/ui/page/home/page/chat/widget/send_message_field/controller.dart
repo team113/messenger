b8a28678358b7b54ddd531cf67b8588c5d1fee93
skazkiful <qwe1233qwe123@gmail.com>
2022-11-29 17:26:58 +0200

    changes

---
+++ lib/ui/page/home/page/chat/widget/send_message_field/controller.dart
@@ -0,0 +1,150 @@
+// Copyright © 2022 IT ENGINEERING MANAGEMENT INC, <https://github.com/team113>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License v3.0 as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License v3.0 for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License v3.0
+// along with this program. If not, see
+// <https://www.gnu.org/licenses/agpl-3.0.html>.
+
+import 'dart:async';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:messenger/domain/model/sending_status.dart';
+import 'package:messenger/util/platform_utils.dart';
+
+import '../../../../../../../domain/model/attachment.dart';
+import '/api/backend/schema.dart' show CreateChatDirectLinkErrorCode;
+import '/config.dart';
+import '/domain/model/chat.dart';
+import '/domain/model/native_file.dart';
+import '/domain/model/user.dart';
+import '/domain/repository/chat.dart';
+import '/domain/service/auth.dart';
+import '/domain/service/chat.dart';
+import '/l10n/l10n.dart';
+import '/provider/gql/exceptions.dart';
+import '/routes.dart';
+import '/ui/widget/text_field.dart';
+import '/util/message_popup.dart';
+
+export 'view.dart';
+
+/// Controller of the [Routes.chatInfo] page.
+class SendMessageFieldController extends GetxController {
+  SendMessageFieldController(
+    this._chatService,
+  );
+
+  /// [Chat]s service used to get [chat] value.
+  final ChatService _chatService;
+
+  /// Maximum allowed [NativeFile.size] of an [Attachment].
+  static const int maxAttachmentSize = 15 * 1024 * 1024;
+
+  /// Opens a media choose popup and adds the selected files to the
+  /// [attachments].
+  Future<void> pickMedia() =>
+      _pickAttachment(PlatformUtils.isIOS ? FileType.media : FileType.image);
+
+  /// Opens the camera app and adds the captured image to the [attachments].
+  Future<void> pickImageFromCamera() async {
+    // TODO: Remove the limitations when bigger files are supported on backend.
+    final XFile? photo = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      maxWidth: 1920,
+      maxHeight: 1920,
+      imageQuality: 90,
+    );
+
+    if (photo != null) {
+      _addXFileAttachment(photo);
+    }
+  }
+
+  /// Opens the camera app and adds the captured video to the [attachments].
+  Future<void> pickVideoFromCamera() async {
+    // TODO: Remove the limitations when bigger files are supported on backend.
+    final XFile? video = await ImagePicker().pickVideo(
+      source: ImageSource.camera,
+      maxDuration: const Duration(seconds: 15),
+    );
+
+    if (video != null) {
+      _addXFileAttachment(video);
+    }
+  }
+
+  /// Opens a file choose popup and adds the selected files to the
+  /// [attachments].
+  Future<void> pickFile() => _pickAttachment(FileType.any);
+
+  /// Opens a file choose popup of the specified [type] and adds the selected
+  /// files to the [attachments].
+  Future<void> _pickAttachment(FileType type) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: type,
+      allowMultiple: true,
+      withReadStream: true,
+    );
+
+    if (result != null && result.files.isNotEmpty) {
+      for (PlatformFile e in result.files) {
+        addPlatformAttachment(e);
+      }
+    }
+  }
+
+  /// Constructs a [NativeFile] from the specified [PlatformFile] and adds it
+  /// to the [attachments].
+  @visibleForTesting
+  Future<void> addPlatformAttachment(PlatformFile platformFile) async {
+    NativeFile nativeFile = NativeFile.fromPlatformFile(platformFile);
+    await _addAttachment(nativeFile);
+  }
+
+  /// Constructs a [NativeFile] from the specified [XFile] and adds it to the
+  /// [attachments].
+  Future<void> _addXFileAttachment(XFile xFile) async {
+    NativeFile nativeFile = NativeFile.fromXFile(xFile, await xFile.length());
+    await _addAttachment(nativeFile);
+  }
+
+  /// Constructs a [LocalAttachment] from the specified [file] and adds it to
+  /// the [attachments] list.
+  ///
+  /// May be used to test a [file] upload since [FilePicker] can't be mocked.
+  Future<void> _addAttachment(NativeFile file) async {
+    if (file.size < maxAttachmentSize) {
+      try {
+        var attachment = LocalAttachment(file, status: SendingStatus.sending);
+        attachments.add(MapEntry(GlobalKey(), attachment));
+
+        Attachment uploaded = await _chatService.uploadAttachment(attachment);
+
+        int index = attachments.indexWhere((e) => e.value.id == attachment.id);
+        if (index != -1) {
+          attachments[index] = MapEntry(attachments[index].key, uploaded);
+          updateDraft();
+        }
+      } on UploadAttachmentException catch (e) {
+        MessagePopup.error(e);
+      } on ConnectionException {
+        // No-op.
+      }
+    } else {
+      MessagePopup.error('err_size_too_big'.l10n);
+    }
+  }
+}
