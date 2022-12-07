e0c9a4f089a73246619a73b253418eb15b03ba4f
SleepySquash <nordnikita@icloud.com>
2022-12-07 11:59:41 +0300

    Fix `Call` notifications not disappearing

--- lib/store/call.dart
+++ lib/store/call.dart
@@ -31,7 +31,6 @@ import '/domain/model/ongoing_call.dart';
 import '/domain/model/user.dart';
 import '/domain/repository/call.dart';
 import '/domain/repository/settings.dart';
-import '/domain/service/disposable_service.dart';
 import '/provider/gql/exceptions.dart';
 import '/provider/gql/graphql.dart';
 import '/provider/hive/chat_call_credentials.dart';
@@ -43,7 +42,7 @@ import 'event/chat_call.dart';
 import 'event/incoming_chat_call.dart';
 
 /// Implementation of an [AbstractCallRepository].
-class CallRepository extends DisposableService
+class CallRepository extends DisposableInterface
     implements AbstractCallRepository {
   CallRepository(
     this._graphQlProvider,
@@ -599,7 +598,14 @@ class CallRepository extends DisposableService
             break;
 
           case IncomingChatCallsTopEventKind.removed:
-            // No-op.
+            e as EventIncomingChatCallsTopChatCallRemoved;
+            final Rx<OngoingCall>? call = calls[e.call.chatId];
+            // If call is not yet connected to the remote updates, then it's
+            // still just a notification and it should be removed.
+            if (call?.value.connected == false &&
+                call?.value.isActive == false) {
+              remove(e.call.chatId);
+            }
             break;
         }
       },
