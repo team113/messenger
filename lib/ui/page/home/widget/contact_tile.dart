5d213b062f34725cd26bd45677103a22de61f6fc
Andrei Ostroverhov <ostroverhov.andrei@gmail.com>
2022-12-14 17:10:49 +0300

    Upgrade contact tile

--- lib/ui/page/home/widget/contact_tile.dart
+++ lib/ui/page/home/widget/contact_tile.dart
@@ -22,6 +22,7 @@ import '/domain/repository/contact.dart';
 import '/domain/repository/user.dart';
 import '/l10n/l10n.dart';
 import '/themes.dart';
+import '/ui/page/home/tab/chats/widget/hovered_ink.dart';
 import '/ui/page/home/widget/avatar.dart';
 import '/ui/widget/context_menu/menu.dart';
 import '/ui/widget/context_menu/region.dart';
@@ -38,15 +39,18 @@ class ContactTile extends StatelessWidget {
     this.leading = const [],
     this.trailing = const [],
     this.onTap,
-    this.actions = const [],
     this.selected = false,
     this.subtitle = const [],
     this.darken = 0,
-    this.folded = false,
     this.height = 86,
     this.radius = 30,
+    this.actions,
+    this.folded = false,
+    this.preventContextMenu = false,
+    this.margin = const EdgeInsets.symmetric(vertical: 3),
   }) : super(key: key);
 
+  /// [MyUser] to display.
   final MyUser? myUser;
 
   /// [RxChatContact] to display.
@@ -64,9 +68,6 @@ class ContactTile extends StatelessWidget {
   /// Callback, called when this [Widget] is tapped.
   final void Function()? onTap;
 
-  /// [ContextMenuRegion.actions] of this [ChatTile].
-  final List<ContextMenuButton> actions;
-
   /// Indicator whether this [ContactTile] is selected.
   final bool selected;
 
@@ -75,6 +76,17 @@ class ContactTile extends StatelessWidget {
 
   /// Indicator whether this [ContactTile] should have its corner folded.
   final bool folded;
+
+  /// Indicator whether a default context menu should be prevented or not.
+  ///
+  /// Only effective under the web, since only web has a default context menu.
+  final bool preventContextMenu;
+
+  /// [ContextMenuRegion.actions] of this [ContactTile].
+  final List<ContextMenuButton>? actions;
+
+  /// Margin to apply to this [ContactTile].
+  final EdgeInsets margin;
 
   /// Optional subtitle [Widget]s.
   final List<Widget> subtitle;
@@ -93,80 +105,75 @@ class ContactTile extends StatelessWidget {
       key: contact != null || user != null
           ? Key('ContextMenuRegion_${contact?.id ?? user?.id ?? myUser?.id}')
           : null,
-      child: Stack(
-        children: [
-          Container(
-            constraints: const BoxConstraints(minHeight: 76),
-            decoration: BoxDecoration(
-              borderRadius: style.cardRadius,
-              border: style.cardBorder,
-              color: Colors.transparent,
-            ),
-            child: Material(
-              type: MaterialType.card,
-              borderRadius: style.cardRadius,
-              color: selected
-                  ? const Color(0xFFD7ECFF).withOpacity(0.8)
-                  : style.cardColor.darken(darken),
-              child: InkWell(
-                borderRadius: style.cardRadius,
-                onTap: onTap,
-                hoverColor: selected
-                    ? const Color(0x00D7ECFF)
-                    : const Color.fromARGB(255, 244, 249, 255),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(12, 12, 12, 12),
-                  child: Row(
+      preventContextMenu: preventContextMenu,
+      actions: actions ?? [],
+      child: Padding(
+        padding: margin,
+        child: InkWellWithHover(
+          selectedColor: style.cardSelectedColor,
+          unselectedColor: style.cardColor.darken(darken),
+          selected: selected,
+          hoveredBorder:
+              selected ? style.primaryBorder : style.cardHoveredBorder,
+          border: selected ? style.primaryBorder : style.cardBorder,
+          borderRadius: style.cardRadius,
+          onTap: onTap,
+          unselectedHoverColor: style.cardHoveredColor.darken(darken),
+          selectedHoverColor: style.cardSelectedColor,
+          folded: contact?.contact.value.favoritePosition != null,
+          child: Padding(
+            key: contact?.contact.value.favoritePosition != null
+                ? Key('FavoriteIndicator_${contact?.contact.value.id}')
+                : null,
+            padding: const EdgeInsets.fromLTRB(12, 14, 12, 14),
+            child: Row(
+              children: [
+                ...leading,
+                if (contact != null)
+                  AvatarWidget.fromRxContact(contact, radius: radius)
+                else if (user != null)
+                  AvatarWidget.fromRxUser(user, radius: radius)
+                else
+                  AvatarWidget.fromMyUser(
+                    myUser,
+                    radius: radius,
+                  ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      ...leading,
-                      if (contact != null)
-                        AvatarWidget.fromRxContact(contact, radius: radius)
-                      else if (user != null)
-                        AvatarWidget.fromRxUser(user, radius: radius)
-                      else
-                        AvatarWidget.fromMyUser(myUser, radius: radius),
-                      const SizedBox(width: 12),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Row(
-                              children: [
-                                Expanded(
-                                  child: Text(
-                                    contact?.contact.value.name.val ??
-                                        contact?.user.value?.user.value.name
-                                            ?.val ??
-                                        contact
-                                            ?.user.value?.user.value.num.val ??
-                                        user?.user.value.name?.val ??
-                                        user?.user.value.num.val ??
-                                        myUser?.name?.val ??
-                                        myUser?.num.val ??
-                                        (myUser == null
-                                            ? '...'
-                                            : 'btn_your_profile'.l10n),
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    style:
-                                        Theme.of(context).textTheme.headline5,
-                                  ),
-                                ),
-                              ],
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              contact?.contact.value.name.val ??
+                                  contact?.user.value?.user.value.name?.val ??
+                                  contact?.user.value?.user.value.num.val ??
+                                  user?.user.value.name?.val ??
+                                  user?.user.value.num.val ??
+                                  myUser?.name?.val ??
+                                  myUser?.num.val ??
+                                  (myUser == null
+                                      ? '...'
+                                      : 'btn_your_profile'.l10n),
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 1,
+                              style: Theme.of(context).textTheme.headline5,
                             ),
-                            ...subtitle,
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      ...trailing,
+                      ...subtitle,
                     ],
                   ),
                 ),
-              ),
+                ...trailing,
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
