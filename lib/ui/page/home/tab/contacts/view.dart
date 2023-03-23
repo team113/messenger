9b02e369b54a9887e6dc194e86d7091861957d01
George <zhorenty@gmail.com>
2023-03-23 14:35:42 +0300

    some changes

--- lib/ui/page/home/tab/contacts/view.dart
+++ lib/ui/page/home/tab/contacts/view.dart
@@ -21,11 +21,11 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:get/get.dart';
+import 'package:messenger/themes.dart';
 
 import '/domain/repository/contact.dart';
 import '/l10n/l10n.dart';
 import '/routes.dart';
-import '/themes.dart';
 import '/ui/page/home/page/chat/message_field/view.dart';
 import '/ui/page/home/page/user/controller.dart';
 import '/ui/page/home/tab/chats/controller.dart';
