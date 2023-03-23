4a6ea55ff97a27fd7b2072178a42944d2bb6c7e7
SleepySquash <nordnikita@icloud.com>
2023-03-23 10:14:23 +0300

    Dummy commit

--- lib/domain/model/ongoing_call.dart
+++ lib/domain/model/ongoing_call.dart
@@ -1411,7 +1411,7 @@ class OngoingCall {
   void _removeLocalTracks(MediaKind kind, MediaSourceKind source) {
     members[_me]?.tracks.removeWhere((t) {
       if (t.kind == kind && t.source == source) {
-        t.dispose(); s
+        t.dispose();
         return true;
       }
       return false;
