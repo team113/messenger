5bafc52adfb785dcf452188e3fdf09e91f4d5f72
SleepySquash <nordnikita@icloud.com>
2023-03-27 11:34:19 +0300

    Impl removing from `Call` on mobile

--- lib/ui/page/call/component/desktop.dart
+++ lib/ui/page/call/component/desktop.dart
@@ -127,10 +127,12 @@ Widget desktopCall(CallController c, BuildContext context) {
                           final bool isOutgoing = (c.outgoing ||
                                   c.state.value == OngoingCallState.local) &&
                               !c.started;
+
                           final bool isIncoming =
                               c.state.value != OngoingCallState.active &&
                                   c.state.value != OngoingCallState.joining &&
                                   !isOutgoing;
+
                           final bool isDialog =
                               c.chat.value?.chat.value.isDialog == true;
 
