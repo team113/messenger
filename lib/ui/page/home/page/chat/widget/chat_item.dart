97801d298e227e8bbe059aaba052f733f1015b22
krida2000 <andriykolesnuk2000@gmail.com>
2023-03-29 11:16:39 +0300

    Fix fmt & copyright

--- lib/ui/page/home/page/chat/widget/chat_item.dart
+++ lib/ui/page/home/page/chat/widget/chat_item.dart
@@ -870,7 +870,8 @@ class _ChatItemWidgetState extends State<ChatItemWidget> {
                     duration: const Duration(milliseconds: 500),
                     opacity: _isRead || !_fromMe ? 1 : 0.55,
                     child: WidgetButton(
-                      onPressed: menu ? null : () => widget.onRepliedTap?.call(e),
+                      onPressed:
+                          menu ? null : () => widget.onRepliedTap?.call(e),
                       child: _repliedMessage(e),
                     ),
                   ),
