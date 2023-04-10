0487a88520499cfaa9fe461e50d90c0ef44d879d
George <zhorenty@gmail.com>
2023-04-10 10:24:17 +0300

    little refactor message_field

--- lib/ui/page/home/page/chat/message_field/view.dart
+++ lib/ui/page/home/page/chat/message_field/view.dart
@@ -719,6 +719,7 @@ class _BuildHeader extends StatelessWidget {
   }
 }
 
+// Builds the visual representation of the provided [Attachment] itself.
 class MessageFieldContent extends StatelessWidget {
   final MapEntry<GlobalKey, Attachment> entry;
   final MessageFieldController c;
@@ -886,247 +887,18 @@ class _BuildAttachment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Attachment e = entry.value;
-    final GlobalKey key = entry.key;
-
-    final bool isImage =
-        (e is ImageAttachment || (e is LocalAttachment && e.file.isImage));
-    final bool isVideo = (e is FileAttachment && e.isVideo) ||
-        (e is LocalAttachment && e.file.isVideo);
 
     const double size = 125;
-
-    // Builds the visual representation of the provided [Attachment] itself.
-    Widget content() {
-      if (isImage || isVideo) {
-        final Widget child = MediaAttachment(
-          attachment: e,
-          width: size,
-          height: size,
-          fit: BoxFit.cover,
-        );
-
-        final List<Attachment> attachments = c.attachments
-            .where((e) {
-              final Attachment a = e.value;
-              return a is ImageAttachment ||
-                  (a is FileAttachment && a.isVideo) ||
-                  (a is LocalAttachment && (a.file.isImage || a.file.isVideo));
-            })
-            .map((e) => e.value)
-            .toList();
-
-        return WidgetButton(
-          key: key,
-          onPressed: e is LocalAttachment
-              ? null
-              : () {
-                  final int index =
-                      c.attachments.indexWhere((m) => m.value == e);
-                  if (index != -1) {
-                    GalleryPopup.show(
-                      context: context,
-                      gallery: GalleryPopup(
-                        initial: index,
-                        initialKey: key,
-                        onTrashPressed: (int i) {
-                          c.attachments
-                              .removeWhere((o) => o.value == attachments[i]);
-                        },
-                        children: attachments.map((o) {
-                          if (o is ImageAttachment ||
-                              (o is LocalAttachment && o.file.isImage)) {
-                            return GalleryItem.image(
-                              o.original.url,
-                              o.filename,
-                              size: o.original.size,
-                              checksum: o.original.checksum,
-                            );
-                          }
-                          return GalleryItem.video(
-                            o.original.url,
-                            o.filename,
-                            size: o.original.size,
-                            checksum: o.original.checksum,
-                          );
-                        }).toList(),
-                      ),
-                    );
-                  }
-                },
-          child: isVideo
-              ? IgnorePointer(
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      child,
-                      Container(
-                        width: 60,
-                        height: 60,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Color(0x80000000),
-                        ),
-                        child: const Icon(
-                          Icons.play_arrow,
-                          color: Colors.white,
-                          size: 48,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              : child,
-        );
-      }
-
-      return Container(
-        width: size,
-        height: size,
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 3),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Flexible(
-                    child: Text(
-                      p.basenameWithoutExtension(e.filename),
-                      style: const TextStyle(fontSize: 13),
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                  Text(
-                    p.extension(e.filename),
-                    style: const TextStyle(fontSize: 13),
-                  )
-                ],
-              ),
-            ),
-            const SizedBox(height: 6),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 3),
-              child: Text(
-                'label_kb'.l10nfmt({
-                  'amount': e.original.size == null
-                      ? 'dot'.l10n * 3
-                      : e.original.size! ~/ 1024
-                }),
-                style: TextStyle(
-                  fontSize: 13,
-                  color: Theme.of(context).colorScheme.primary,
-                ),
-                textAlign: TextAlign.center,
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ],
-        ),
-      );
-    }
-
-    Widget attachment() {
-      final Style style = Theme.of(context).extension<Style>()!;
-
-      return MouseRegion(
-        key: Key('Attachment_${e.id}'),
-        opaque: false,
-        onEnter: (_) => c.hoveredAttachment.value = e,
-        onExit: (_) => c.hoveredAttachment.value = null,
-        child: Container(
-          width: size,
-          height: size,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            color: const Color(0xFFF5F5F5),
-          ),
-          margin: const EdgeInsets.symmetric(horizontal: 2),
-          child: Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: content(),
-              ),
-              Center(
-                child: SizedBox.square(
-                  dimension: 30,
-                  child: ElasticAnimatedSwitcher(
-                    child: e is LocalAttachment
-                        ? e.status.value == SendingStatus.error
-                            ? Container(
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.white,
-                                ),
-                                child: const Center(
-                                  child: Icon(Icons.error, color: Colors.red),
-                                ),
-                              )
-                            : const SizedBox()
-                        : const SizedBox(),
-                  ),
-                ),
-              ),
-              if (!c.field.status.value.isLoading)
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Padding(
-                    padding: const EdgeInsets.only(right: 4, top: 4),
-                    child: Obx(() {
-                      final Widget child;
-
-                      if (c.hoveredAttachment.value == e ||
-                          PlatformUtils.isMobile) {
-                        child = InkWell(
-                          key: const Key('RemovePickedFile'),
-                          onTap: () =>
-                              c.attachments.removeWhere((a) => a.value == e),
-                          child: Container(
-                            width: 15,
-                            height: 15,
-                            margin: const EdgeInsets.only(left: 8, bottom: 8),
-                            child: Container(
-                              key: const Key('Close'),
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: style.cardColor,
-                              ),
-                              alignment: Alignment.center,
-                              child: SvgLoader.asset(
-                                'assets/icons/close_primary.svg',
-                                width: 7,
-                                height: 7,
-                              ),
-                            ),
-                          ),
-                        );
-                      } else {
-                        child = const SizedBox();
-                      }
-
-                      return AnimatedSwitcher(
-                        duration: 200.milliseconds,
-                        child: child,
-                      );
-                    }),
-                  ),
-                ),
-            ],
-          ),
-        ),
-      );
-    }
 
     return Dismissible(
       key: Key(e.id.val),
       direction: DismissDirection.up,
       onDismissed: (_) => c.attachments.removeWhere((a) => a.value == e),
-      child: attachment(),
+      child: MessageFieldAttachment(
+        entry: entry,
+        c: c,
+        size: size,
+      ),
     );
   }
 }
@@ -1255,6 +1027,117 @@ class _BuildField extends StatelessWidget {
             );
           }),
         ],
+      ),
+    );
+  }
+}
+
+class MessageFieldAttachment extends StatelessWidget {
+  final MapEntry<GlobalKey, Attachment> entry;
+  final MessageFieldController c;
+  final double size;
+
+  const MessageFieldAttachment({
+    Key? key,
+    required this.entry,
+    required this.c,
+    required this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Style style = Theme.of(context).extension<Style>()!;
+    final Attachment e = entry.value;
+
+    return MouseRegion(
+      key: Key('Attachment_${e.id}'),
+      opaque: false,
+      onEnter: (_) => c.hoveredAttachment.value = e,
+      onExit: (_) => c.hoveredAttachment.value = null,
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color(0xFFF5F5F5),
+        ),
+        margin: const EdgeInsets.symmetric(horizontal: 2),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: MessageFieldContent(
+                entry: entry,
+                c: c,
+                size: size,
+              ),
+            ),
+            Center(
+              child: SizedBox.square(
+                dimension: 30,
+                child: ElasticAnimatedSwitcher(
+                  child: e is LocalAttachment
+                      ? e.status.value == SendingStatus.error
+                          ? Container(
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.white,
+                              ),
+                              child: const Center(
+                                child: Icon(Icons.error, color: Colors.red),
+                              ),
+                            )
+                          : const SizedBox()
+                      : const SizedBox(),
+                ),
+              ),
+            ),
+            if (!c.field.status.value.isLoading)
+              Align(
+                alignment: Alignment.topRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 4, top: 4),
+                  child: Obx(() {
+                    final Widget child;
+
+                    if (c.hoveredAttachment.value == e ||
+                        PlatformUtils.isMobile) {
+                      child = InkWell(
+                        key: const Key('RemovePickedFile'),
+                        onTap: () =>
+                            c.attachments.removeWhere((a) => a.value == e),
+                        child: Container(
+                          width: 15,
+                          height: 15,
+                          margin: const EdgeInsets.only(left: 8, bottom: 8),
+                          child: Container(
+                            key: const Key('Close'),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: style.cardColor,
+                            ),
+                            alignment: Alignment.center,
+                            child: SvgLoader.asset(
+                              'assets/icons/close_primary.svg',
+                              width: 7,
+                              height: 7,
+                            ),
+                          ),
+                        ),
+                      );
+                    } else {
+                      child = const SizedBox();
+                    }
+
+                    return AnimatedSwitcher(
+                      duration: 200.milliseconds,
+                      child: child,
+                    );
+                  }),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
