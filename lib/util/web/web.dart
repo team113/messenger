4d90167b14423dffd3ba089098d56d493d8fcf89
krida2000 <71345244+krida2000@users.noreply.github.com>
2023-04-14 20:20:01 +0300

    Display alert on Web favicon when `MyUser.unreadChatsCount` is non-zero (#403)

--- lib/util/web/web.dart
+++ lib/util/web/web.dart
@@ -552,6 +552,22 @@ class WebUtils {
       Uri.base.toString().replaceFirst(from, to),
     );
   }
+
+  /// Sets the favicon being used to an alert style.
+  static void setAlertFavicon() {
+    for (html.LinkElement e in html.querySelectorAll("link[rel*='icon']")) {
+      if (!e.href.contains('icons/alert/')) {
+        e.href = e.href.replaceFirst('icons/', 'icons/alert/');
+      }
+    }
+  }
+
+  /// Sets the favicon being used to the default style.
+  static void setDefaultFavicon() {
+    for (html.LinkElement e in html.querySelectorAll("link[rel*='icon']")) {
+      e.href = e.href.replaceFirst('icons/alert/', 'icons/');
+    }
+  }
 }
 
 /// Extension adding JSON manipulation methods to a [Rect].
