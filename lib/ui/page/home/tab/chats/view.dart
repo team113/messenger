3a8e19fdeedd1a2dc53e5ceebe9e6957abc319c1
George <zhorenty@gmail.com>
2023-04-24 10:33:04 +0300

    refactor

--- lib/ui/page/home/tab/chats/view.dart
+++ lib/ui/page/home/tab/chats/view.dart
@@ -198,8 +198,8 @@ class ChatsTabView extends StatelessWidget {
 
                       if (c.searching.value) {
                         child = SvgLoader.asset(
-                          key: const Key('CloseSearch'),
                           'assets/icons/close_primary.svg',
+                          key: const Key('CloseSearch'),
                           height: 15,
                         );
                       } else {
