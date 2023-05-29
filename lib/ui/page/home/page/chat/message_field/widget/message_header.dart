20097a6464417ecc8159f0bd8e7a420cd6489a9f
George <zhorenty@gmail.com>
2023-05-29 10:10:50 +0300

    docs and copyright fix

--- lib/ui/page/home/page/chat/message_field/widget/message_header.dart
+++ lib/ui/page/home/page/chat/message_field/widget/message_header.dart
@@ -1,3 +1,20 @@
+// Copyright © 2022-2023 IT ENGINEERING MANAGEMENT INC,
+//                       <https://github.com/team113>
+//
+// This program is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Affero General Public License v3.0 as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License v3.0 for
+// more details.
+//
+// You should have received a copy of the GNU Affero General Public License v3.0
+// along with this program. If not, see
+// <https://www.gnu.org/licenses/agpl-3.0.html>.
+
 import 'dart:math';
 import 'dart:ui';
 
@@ -77,7 +94,7 @@ class MessageHeader extends StatelessWidget {
   /// Replied [ChatItem] being hovered.
   final Rx<ChatItem?> hoveredReply;
 
-  /// Returns an [User] from [UserService] by the provided [id].
+  /// Returns an [User] from [UserService] by the provided id.
   final Future<RxUser?> Function(UserId id) getUser;
 
   /// Callback, called when a [ChatItem] being a reply or edited is pressed.
