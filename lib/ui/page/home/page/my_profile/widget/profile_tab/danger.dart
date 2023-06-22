1a3484a36b0a220e2fe30a9022fccc60854102db
George <zhorenty@gmail.com>
2023-06-22 15:56:44 +0300

    fix l10n import

--- lib/ui/page/home/page/my_profile/widget/profile_tab/danger.dart
+++ lib/ui/page/home/page/my_profile/widget/profile_tab/danger.dart
@@ -16,8 +16,8 @@
 // <https://www.gnu.org/licenses/agpl-3.0.html>.
 
 import 'package:flutter/material.dart';
-import 'package:messenger/l10n/l10n.dart';
 
+import '/l10n/l10n.dart';
 import '/themes.dart';
 import '/ui/page/home/widget/field_button.dart';
 import '/ui/page/home/widget/paddings.dart';
