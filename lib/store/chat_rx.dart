5eaa774665c0515df85adb48657416f551b895ee
krida2000 <71345244+krida2000@users.noreply.github.com>
2024-05-09 11:58:17 +0300

    Don't persist `Chat`s to Hive in popup `Call`s (#990, #691)

--- lib/store/chat_rx.dart
+++ lib/store/chat_rx.dart
@@ -42,6 +42,7 @@ import '/domain/repository/paginated.dart';
 import '/domain/repository/user.dart';
 import '/provider/gql/exceptions.dart'
     show ConnectionException, PostChatMessageException, StaleVersionException;
+import '/provider/hive/base.dart';
 import '/provider/hive/chat.dart';
 import '/provider/hive/chat_item.dart';
 import '/provider/hive/chat_item_sorting.dart';
@@ -807,7 +808,7 @@ class HiveRxChat extends RxChat {
           chatEntity?.lastItemCursor = null;
         }
 
-        await txn.put(chatEntity!.value.id.val, chatEntity);
+        await _putChat(chatEntity!, txn);
       }
     });
   }
@@ -971,7 +972,7 @@ class HiveRxChat extends RxChat {
         // TODO: Avatar should be updated by [Hive] subscription.
         this.avatar.value = avatar;
 
-        await txn.put(chatEntity.value.id.val, chatEntity);
+        await _putChat(chatEntity, txn);
       }
     });
   }
@@ -1017,7 +1018,7 @@ class HiveRxChat extends RxChat {
                   firstItem != null &&
                   chatEntity.value.firstItem != firstItem) {
                 chatEntity.value.firstItem = firstItem;
-                await txn.put(chatEntity.value.id.val, chatEntity);
+                await _putChat(chatEntity, txn);
               }
             });
           }
@@ -1318,7 +1319,7 @@ class HiveRxChat extends RxChat {
             final HiveChat? chat = await txn.get(id.val);
             if (chat != null) {
               chat.value.muted = null;
-              await txn.put(chat.value.id.val, chat);
+              await _putChat(chat, txn);
             }
           });
         },
@@ -1531,9 +1532,9 @@ class HiveRxChat extends RxChat {
           if (chatEntity != null) {
             chatEntity.value = node.chat.value;
             chatEntity.ver = node.chat.ver;
-            await txn.put(chatEntity.value.id.val, chatEntity);
+            await _putChat(chatEntity, txn);
           } else {
-            await txn.put(node.chat.value.id.val, node.chat);
+            await _putChat(node.chat, txn);
           }
         });
 
@@ -1553,6 +1554,13 @@ class HiveRxChat extends RxChat {
               '$runtimeType($id)',
             );
 
+            // Be sure to keep the [chat] up to date with the [chatEntity], when
+            // in [WebUtils.isPopup], as in such cases local Hive subscriptions
+            // might not work due to Hive restrictions in multiple Web tabs.
+            if (WebUtils.isPopup) {
+              chat.value = chatEntity?.value ?? chat.value;
+            }
+
             return;
           }
 
@@ -1562,6 +1570,14 @@ class HiveRxChat extends RxChat {
           );
 
           chatEntity.ver = versioned.ver;
+
+          // Use the [chat] value instead of [chatEntity], when in
+          // [WebUtils.isPopup], as in such cases we can't rely on [Hive] having
+          // actual data (especially when multiple events are received one after
+          // another).
+          if (WebUtils.isPopup) {
+            chatEntity.value = chat.value;
+          }
 
           bool shouldPutChat = subscribed;
 
@@ -1960,10 +1976,24 @@ class HiveRxChat extends RxChat {
           }
 
           if (shouldPutChat) {
-            await txn.put(chatEntity.value.id.val, chatEntity);
+            await _putChat(chatEntity, txn);
           }
         });
         break;
+    }
+  }
+
+  /// Puts the provided [chat] to the [Hive] using the provided [txn].
+  Future<void> _putChat(HiveChat chat, HiveTransaction<HiveChat> txn) async {
+    // TODO: Don't write to [Hive] from popup, as [Hive] doesn't support isolate
+    //       synchronization, thus writes from multiple applications may lead to
+    //       missing events:
+    //       https://github.com/team113/messenger/issues/27
+    if (WebUtils.isPopup) {
+      this.chat.value = chat.value;
+      this.chat.refresh();
+    } else {
+      await txn.put(chat.value.id.val, chat);
     }
   }
 }
