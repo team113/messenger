0ad97f29c3dc618993579877798da45ef667c5e0
SleepySquash <nordnikita@icloud.com>
2024-05-15 16:47:19 +0300

    Ugh, try fixing `BuildContext` being `null` issues

--- test/e2e/steps/users.dart
+++ test/e2e/steps/users.dart
@@ -51,10 +51,13 @@ final StepDefinitionGeneric iAm = given1<TestUser, CustomWorld>(
     router.home();
 
     // Ensure business logic is initialized.
-    await context.world.appDriver.waitUntil(() async {
-      return Get.isRegistered<ChatService>() &&
-          Get.isRegistered<MyUserService>();
-    });
+    await context.world.appDriver.waitUntil(
+      () async {
+        return Get.isRegistered<ChatService>() &&
+            Get.isRegistered<MyUserService>();
+      },
+      timeout: const Duration(seconds: 30),
+    );
   },
   configuration: StepDefinitionConfiguration()
     ..timeout = const Duration(minutes: 5),
