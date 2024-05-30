e435c4454fb120c058589e1f854cf9412a5e7f72
SleepySquash <nordnikita@icloud.com>
2024-05-30 15:13:40 +0300

    Remove `Log.debug`

--- lib/store/chat_rx.dart
+++ lib/store/chat_rx.dart
@@ -1454,7 +1454,7 @@ class RxChatImpl extends RxChat {
   void _initLocalSubscription() {
     _localSubscription?.cancel();
     _localSubscription = _driftChat.watch(id).listen((e) {
-      Log.debug('got: $e', '$runtimeType($id)');
+      // Log.debug('got: $e', '$runtimeType($id)');
 
       if (e != null) {
         final ChatItem? first = chat.value.firstItem;
