33f3bc348c8273418e1f9bd9a2e9b1787093f4fe
Gieted <gieted@proton.me>
2024-09-24 00:05:39 +0200

    Create header

--- lib/ui/page/home/page/my_profile/avatar_cropper.dart
+++ lib/ui/page/home/page/my_profile/avatar_cropper.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../../../domain/model/native_file.dart';
+import '../../../../../themes.dart';
+import '../../../../widget/svg/svgs.dart';
+import '../../../call/widget/round_button.dart';
 
 class AvatarCropper extends StatefulWidget {
   const AvatarCropper({super.key, required this.file});
@@ -16,6 +19,7 @@ class AvatarCropper extends StatefulWidget {
 
 class _AvatarCropperState extends State<AvatarCropper> {
   final fileContent = Rx<Uint8List?>(null);
+  final hovered = Rx(false);
 
   @override
   void initState() {
@@ -28,23 +32,67 @@ class _AvatarCropperState extends State<AvatarCropper> {
 
   @override
   Widget build(BuildContext context) {
+    final navigator = Navigator.of(context, rootNavigator: true);
+    final style = Theme.of(context).style;
+
     return Obx(() {
       return Container(
         color: Colors.black,
-        child: Stack(children: [
-          Stack(children: [
-            if (fileContent() != null) Image.memory(fileContent()!),
+        alignment: Alignment.center,
+        child: Column(children: [
+          const SizedBox(height: 30),
+          Row(mainAxisSize: MainAxisSize.min, children: [
+            Container(
+              width: 100,
+              alignment: Alignment.centerLeft,
+              child: SizedBox(
+                width: 60,
+                height: 60,
+                child: MouseRegion(
+                  onEnter: (_) => hovered.value = true,
+                  onExit: (_) => hovered.value = false,
+                  child: ClipRRect(
+                    child: RoundFloatingButton(
+                      color: hovered()
+                          ? Colors.white.withOpacity(0.2)
+                          : Colors.transparent,
+                      onPressed: navigator.pop,
+                      icon: SvgIcons.arrowLeft,
+                      offset: const Offset(-1, 0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Text(
+              'Crop your image',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              softWrap: false,
+            ),
+            Container(
+              width: 100,
+              alignment: Alignment.centerRight - const Alignment(0.2, 0),
+              child: Text(
+                'Save',
+                style: TextStyle(color: style.colors.primary, fontSize: 20),
+              ),
+            ),
           ]),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Listener(
-              onPointerDown: (_) {
-                final navigator = Navigator.of(context, rootNavigator: true);
-                navigator.pop();
-              },
-              child: const Text(
-                'close',
-                style: TextStyle(color: Colors.white),
+          Expanded(
+            child: Center(
+              child: SizedBox(
+                width: 500,
+                height: 500,
+                child: Stack(children: [
+                  if (fileContent() != null)
+                    Center(
+                      child: Image.memory(fileContent()!, fit: BoxFit.contain),
+                    ),
+                ]),
               ),
             ),
           ),
