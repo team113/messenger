dad69c9652f7b0c226e23cbca2260c03f40ee3a7
SleepySquash <nordnikita@icloud.com>
2024-09-30 17:47:11 +0300

    Try

--- test/e2e/configuration.dart
+++ test/e2e/configuration.dart
@@ -359,7 +359,6 @@ Future<void> appInitializationFn(World world) {
   Get.put<GeoLocationProvider>(MockGeoLocationProvider());
   Get.put<GraphQlProvider>(MockGraphQlProvider());
 
-  final Function(FlutterErrorDetails)? presentError = FlutterError.onError;
   FlutterError.onError = (details) {
     final String exception = details.exception.toString();
 
@@ -382,7 +381,7 @@ Future<void> appInitializationFn(World world) {
       return;
     }
 
-    presentError?.call(details);
+    FlutterError.presentError(details);
   };
 
   return Future.sync(app.main);
