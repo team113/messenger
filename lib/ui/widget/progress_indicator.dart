48b4f60065c8df3cabbb180f4d678fd2a8f9cd56
SleepySquash <nordnikita@icloud.com>
2025-02-13 11:54:11 +0300

    Upgrade dependencies

--- lib/ui/widget/progress_indicator.dart
+++ lib/ui/widget/progress_indicator.dart
@@ -351,7 +351,7 @@ class _CircularProgressIndicatorState
           tailValue: tailValue,
           offsetValue: offsetValue,
           rotationValue: rotationValue,
-          strokeWidth: widget.strokeWidth,
+          strokeWidth: widget.strokeWidth ?? 2,
         ),
       ),
     );
