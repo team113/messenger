6503740e2cbba1f58f104422323213e91de0ab6c
SleepySquash <nordnikita@icloud.com>
2025-05-01 16:40:34 +0300

    Bootstrap

--- lib/domain/service/auth.dart
+++ lib/domain/service/auth.dart
@@ -20,8 +20,10 @@ import 'dart:convert';
 
 import 'package:collection/collection.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart' show visibleForTesting;
+import 'package:flutter/material.dart'
+    show AppLifecycleState, visibleForTesting;
 import 'package:get/get.dart';
+import 'package:mutex/mutex.dart';
 
 import '/config.dart';
 import '/domain/model/chat.dart';
@@ -90,14 +92,21 @@ class AuthService extends DisposableService {
   final Map<UserId, Timer> _refreshTimers = {};
 
   /// [_refreshTimers] interval.
-  final Duration _refreshTaskInterval = const Duration(seconds: 30);
+  final Duration _refreshTaskInterval = const Duration(seconds: 10);
 
   /// Minimal allowed [credentials] TTL.
-  final Duration _accessTokenMinTtl = const Duration(minutes: 2);
+  final Duration _accessTokenMinTtl = const Duration(minutes: 29);
 
   /// [StreamSubscription] to [WebUtils.onStorageChange] fetching new
   /// [Credentials].
   StreamSubscription? _storageSubscription;
+
+  /// [Mutex] being unlocked only when [AppLifecycleState] is in foreground.
+  final Mutex _lifecycleMutex = Mutex();
+
+  /// [Worker] reacting on the [RouterState.lifecycle] changes to lock/unlock
+  /// the [_lifecycleMutex].
+  Worker? _lifecycleWorker;
 
   /// [refreshSession] attempt number counter used purely for [Log]s.
   static int _refreshAttempt = 0;
@@ -124,6 +133,7 @@ class AuthService extends DisposableService {
     Log.debug('onClose()', '$runtimeType');
 
     _storageSubscription?.cancel();
+    _lifecycleWorker?.dispose();
     _refreshTimers.forEach((_, t) => t.cancel());
     _refreshTimers.clear();
 
@@ -204,6 +214,27 @@ class AuthService extends DisposableService {
             router.go(_unauthorized());
           }
         }
+      }
+    });
+
+    _lifecycleWorker = ever(router.lifecycle, (AppLifecycleState state) async {
+      Log.debug('_lifecycleWorker -> ${state.name}', '$runtimeType');
+
+      switch (state) {
+        case AppLifecycleState.resumed:
+        case AppLifecycleState.inactive:
+          if (_lifecycleMutex.isLocked) {
+            _lifecycleMutex.release();
+          }
+          break;
+
+        case AppLifecycleState.detached:
+        case AppLifecycleState.hidden:
+        case AppLifecycleState.paused:
+          if (!_lifecycleMutex.isLocked) {
+            await _lifecycleMutex.acquire();
+          }
+          break;
       }
     });
 
@@ -627,6 +658,23 @@ class AuthService extends DisposableService {
   /// the active one, if [userId] is not provided.
   Future<void> refreshSession({UserId? userId}) async {
     final int attempt = _refreshAttempt++;
+
+    if (!router.lifecycle.value.inForeground) {
+      Log.debug(
+        'refreshSession($userId |-> $attempt) waiting for application to be active...',
+        '$runtimeType',
+      );
+
+      await _lifecycleMutex.acquire();
+      Log.debug(
+        'refreshSession($userId |-> $attempt) waiting for application to be active... done! ✨',
+        '$runtimeType',
+      );
+
+      if (_lifecycleMutex.isLocked) {
+        _lifecycleMutex.release();
+      }
+    }
 
     final FutureOr<bool> futureOrBool = WebUtils.isLocked;
     final bool isLocked =
