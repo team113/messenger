7f830553a049c2f893d92d06700b4d7218d89d35
ChabanovX <firstchabanov@gmail.com>
2025-07-10 00:53:16 +0300

    Added raw node checking

--- tools/svg_checker/parse_project_assets.dart
+++ tools/svg_checker/parse_project_assets.dart
@@ -1,13 +1,27 @@
 import 'dart:io';
 
+import 'package:analyzer/dart/analysis/features.dart';
+import 'package:analyzer/dart/analysis/utilities.dart';
+import 'package:analyzer/dart/ast/visitor.dart';
+import 'package:analyzer/src/dart/ast/ast.dart';
+
 void main() {
-  parseProjectSvgAssets('assets');
+  final svgIconsPath =
+      r'/Users/ivanchabanov/prog/money/temp/messenger/lib/ui/widget/svg/svgs.dart';
+  parseSvgClass(svgIconsPath);
 }
 
-Future<Set<String>> parseSvgClass(String path) async {
-  final Set<String> svgAssets;
+Future<Map<String, String>> parseSvgClass(String path) async {
+  final Map<String, String> svgAssets;
 
-  
+  final fileUnit = parseFile(
+    path: path,
+    featureSet: FeatureSet.latestLanguageVersion(),
+  ).unit;
+
+  final visitor = _DeclaredIconsCollector();
+  fileUnit.visitChildren(visitor);
+  svgAssets = visitor.findings;
 
   return svgAssets;
 }
@@ -22,4 +36,19 @@ Future<Set<String>> parseProjectSvgAssets(String path) async {
       .toSet();
 
   return svgAssets;
+}
+
+class _DeclaredIconsCollector extends RecursiveAstVisitor<void> {
+  final Map<String, String> findings = {};
+
+  @override
+  void visitFieldDeclaration(FieldDeclaration node) {
+    // We only care about: static const SvgData <name> = SvgData(...)
+    if (!node.isStatic || !node.fields.isConst) return;
+
+    final fieldType = node.fields.type?.type;
+
+    print('$node, $fieldType');
+    super.visitFieldDeclaration(node);
+  }
 }
