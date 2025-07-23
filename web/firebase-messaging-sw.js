32a9864a7280f2c3e5e4e0f7fe210946703dcd38
SleepySquash <nordnikita@icloud.com>
2025-07-23 11:44:37 +0300

    Upgrade Firebase SW to 10.13.2

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -15,8 +15,8 @@
 // along with this program. If not, see
 // <https://www.gnu.org/licenses/agpl-3.0.html>.
 
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyBbttYFbYjucn8BY-p5tlWomcd5V9h8zWc",
