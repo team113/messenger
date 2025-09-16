b03d6f2bc1bf67b6da09003c0b7fe94fcb90f106
Peter Parker <petrfalkovsky@yandex.ru>
2025-09-16 14:55:56 +0000

    Revert "Remove unused svg constants from SvgIcons"
    This reverts commit 831b73248b6616f62032e20c1dc11a2b26971659.

--- lib/domain/repository/settings.dart
+++ lib/domain/repository/settings.dart
@@ -53,7 +53,10 @@ abstract class AbstractSettingsRepository {
 
   /// Sets the [MediaSettings.noiseSuppression] and
   /// [MediaSettings.noiseSuppressionLevel] values.
-  Future<void> setNoiseSuppression({bool? enabled, NoiseSuppressionLevel? level});
+  Future<void> setNoiseSuppression({
+    bool? enabled,
+    NoiseSuppressionLevel? level,
+  });
 
   /// Sets the [MediaSettings.echoCancellation] value.
   Future<void> setEchoCancellation(bool enabled);
