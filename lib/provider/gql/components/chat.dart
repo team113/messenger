793fb84e1917f077b1f9b3eae09bbbc7857d31ea
AlexGalitsky <alex.galitsky.kd@gmail.com>
2025-09-16 11:59:21 +0300

    Add `archived` param to `RecentChatsFilter` in GraphQL

--- lib/provider/gql/components/chat.dart
+++ lib/provider/gql/components/chat.dart
@@ -115,7 +115,7 @@ mixin ChatGraphQlMixin {
       ),
       kw$with: RecentChatsFilter(
         noFavorite: noFavorite,
-        archived: archive,
+        archived: archived,
         ongoingCalls: withOngoingCalls,
       ),
     );
@@ -675,6 +675,7 @@ mixin ChatGraphQlMixin {
   Stream<QueryResult> recentChatsTopEvents(
     int count, {
     bool noFavorite = false,
+    bool archived = false,
     bool? withOngoingCalls,
   }) {
     Log.debug(
@@ -687,6 +688,7 @@ mixin ChatGraphQlMixin {
       kw$with: RecentChatsFilter(
         noFavorite: noFavorite,
         ongoingCalls: withOngoingCalls,
+        archived: archived,
       ),
     );
     return client.subscribe(
