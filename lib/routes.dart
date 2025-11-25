a8125614ce6e383abb54346edfceab23de8df95c
Nikita <SleepySquash@users.noreply.github.com>
2025-11-25 11:18:01 +0300

    Fix routes not popping in `router.pop()` by removing `_accounted` list (#1526)

--- lib/routes.dart
+++ lib/routes.dart
@@ -232,9 +232,6 @@ class RouterState extends ChangeNotifier {
   /// Current [Routes.home] tab.
   HomeTab _tab = HomeTab.chats;
 
-  /// List of [routes] already [pop]ped so that those aren't removed twice.
-  final List<String> _accounted = [];
-
   /// Current route (last in the [routes] history).
   String get route => routes.lastOrNull == null ? Routes.home : routes.last;
 
@@ -256,12 +253,6 @@ class RouterState extends ChangeNotifier {
     Log.debug('go($to)', '$runtimeType');
 
     arguments = null;
-
-    for (var e in routes) {
-      if (e != '/' && e != to) {
-        _accounted.add(e);
-      }
-    }
 
     routes.value = [_guarded(to)];
     notifyListeners();
@@ -291,10 +282,6 @@ class RouterState extends ChangeNotifier {
   void pop([String? page]) {
     Log.debug('pop($page)', '$runtimeType');
 
-    if (_accounted.remove(page ?? routes.lastOrNull)) {
-      return;
-    }
-
     if (routes.isNotEmpty) {
       if (page != null && !routes.contains(page)) {
         return;
@@ -312,13 +299,11 @@ class RouterState extends ChangeNotifier {
           last = Routes.home;
         }
 
-        _accounted.remove(routes.last);
         routes.last = last;
       } else {
         if (page != null) {
           routes.remove(page);
         } else {
-          _accounted.remove(routes.last);
           routes.removeLast();
         }
 
@@ -336,7 +321,6 @@ class RouterState extends ChangeNotifier {
     for (String e in routes.toList(growable: false)) {
       if (predicate(e)) {
         routes.remove(route);
-        _accounted.add(route);
       }
     }
 
