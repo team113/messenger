7f2c79676ea84534291bcf9b0c9e3d01bdbb9cf2
Lawrence <SleepySquash@users.noreply.github.com>
2026-01-05 17:51:52 +0500

    Bootstrap sharing of `MediaKind.audio` from `MediaSourceKind.display` source in `OngoingCall` (#1570)

--- lib/util/web/web.dart
+++ lib/util/web/web.dart
@@ -365,6 +365,11 @@ class WebUtils {
       (Config.allowDetachedActivity ||
           router.lifecycle.value != AppLifecycleState.detached);
 
+  /// Indicates whether this platform supports system audio capture.
+  static FutureOr<bool> get canShareAudio {
+    return false;
+  }
+
   /// Removes [Credentials] identified by the provided [UserId] from the
   /// browser's storage.
   static void removeCredentials(UserId userId) {
