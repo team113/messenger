8d186aa02d3cab02bf833e7288070d4386a4407b
SleepySquash <nordnikita@icloud.com>
2026-03-04 15:39:18 +0300

    Bootstrap

--- lib/ui/page/home/page/my_profile/camera_switch/controller.dart
+++ lib/ui/page/home/page/my_profile/camera_switch/controller.dart
@@ -45,7 +45,7 @@ class CameraSwitchController extends GetxController {
   final RxList<DeviceDetails> devices = RxList<DeviceDetails>([]);
 
   /// ID of the initially selected video device.
-  RxnString camera;
+  final RxnString camera;
 
   /// [RtcVideoRenderer] rendering the currently selected [camera] device.
   final Rx<RtcVideoRenderer?> renderer = Rx<RtcVideoRenderer?>(null);
